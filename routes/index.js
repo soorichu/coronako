a383fea52556f1797054422b0bb7bfdc480ed68d
soorichu <soorichu@gmail.com>
2020-02-09 21:29:06 +0900

    hint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home'});
+  res.render('index', {page:'현재 위치', menuId:'home'});
 });
 
 router.get('/about', function(req, res, next) {
