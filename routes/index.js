626e3eb178e8736495643e44030912ef6475357b
soorichu <soorichu@gmail.com>
2020-02-12 01:22:49 +0900

    change

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'현재 위치', menuId:'home'});
+  res.render('index', {page:'지도-내 주변 확진자 동선', menuId:'home'});
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', {page:'About', menuId:'about'});
+  res.render('about', {page:'소개 및 유용한 사이트', menuId:'about'});
 });
 
 router.get('/datas', function(req, res, next) {
-  res.render('datas', {page:'Data', menuId:'datas'});
+  res.render('datas', {page:'데이터-전세계 코로나 바이러스 추이', menuId:'datas'});
 });
 
 router.get('/test', function(req, res, next){
